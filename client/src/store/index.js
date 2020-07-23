30a08f3b8d0efa6559dff61657724b33210ad2c2
heriant0 <herianto890@gmail.com>
2020-07-23 22:52:15 +0700

    Socket Client dan server: + add routing get all user + add routing post user + on client register user + on client display sentences

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import router from '../router/index.js'
 
 Vue.use(Vuex)
-
+const baseUrl = `http://localhost:3000/`
 export default new Vuex.Store({
   state: {
   },
   mutations: {
   },
   actions: {
+    addUser(contex, username) {
+      return axios({
+        method: 'POST',
+        url: `${baseUrl}`,
+        data: {
+          name: username,
+          score: 1000,
+        },
+      })
+        .then((result) => {
+          console.log(result, 'result.data');
+          router.push({path: '/about'})
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
   modules: {
   }
--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+        <h1> {{messages}} </h1>
   </div>
 </template>
+
+<script>
+import io from "socket.io-client";
+const socket = io("http://localhost:3000")
+export default {
+  data() {
+    return {
+      messages: []
+    }
+  },
+  created() {
+    socket.on("sentences", (payload) => {
+      console.log(payload, "listener new message")
+      this.messages = payload.content;
+    });
+  },
+}
+</script>
