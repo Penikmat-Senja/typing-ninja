609097e3e341fb49ea0f870045b8cbd3005dcc67
unknown <azis6229@gmail.com>
2020-07-24 02:59:04 +0700

    add fitur time interval 	modified:   client/src/views/Games.vue 	modified:   client/src/views/Home.vue 	modified:   server/app.js 	modified:   server/config/config.json

--- client/src/views/Games.vue
+++ client/src/views/Games.vue
@@ -1,11 +1,19 @@
 <template>
+    <div v-if="times > 0">
   <div class="about">
+    <h3>{{times}}</h3>
+    <button @click="countDown">play</button>
     <h1>{{messages}}</h1>
     <form @submit.prevent="submitAnswer">
       <input type="text" v-model="inputan" />
 
       <button type="submit">OK</button>
     </form>
+    </div>
+    </div>
+    <div v-else>
+
+<p>cek</p>
   </div>
 </template>
 
@@ -17,24 +25,41 @@ export default {
     return {
       messages: "",
       inputan: "",
+      times: 20,
     };
+       
   },
   methods: {
     submitAnswer() {
       if (this.messages === this.inputan) {
         localStorage.score = Number(localStorage.score) + Number(10);
-        socket.emit("katakata", (payload) => {
+        socket.emit("katakata", payload => {
           //  this.messages = payload.content;
         });
+        this.inputan = "";
       }
       console.log(this.score);
     },
+    countDown() {
+      //  setInterval(( )=> {
+      //   this.times -= 1;
+
+        socket.emit("time", payload => {
+          //  this.messages = payload.content;
+        });
+      // }, 1000);
+      // console.log(this.times);
+    socket.on("time", payload => {
+      console.log(payload, "listener new message");
+      this.times = payload;
+    });
+    }
   },
   created() {
-    socket.on("sentences", (payload) => {
+    socket.on("sentences", payload => {
       console.log(payload, "listener new message");
       this.messages = payload.content;
     });
-  },
+  }
 };
 </script>
